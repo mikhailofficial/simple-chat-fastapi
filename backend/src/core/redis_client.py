93f04532c668db8a759456fadf94944d4a89ae9d
mikhailov <asdgrghd@gmail.com>
2025-08-23 12:00:53 +0400

    feat: dockerized backend application

--- backend/src/core/redis_client.py
+++ backend/src/core/redis_client.py
@@ -1,4 +1,9 @@
 import redis.asyncio as redis
+import os
 
 
-redis_connection = redis.from_url(url="redis://localhost:6379", decode_responses=True)
+redis_host = os.getenv("REDIS_HOST", "host.docker.internal")
+redis_port = os.getenv("REDIS_PORT", "6379")
+redis_url = f"redis://{redis_host}:{redis_port}"
+
+redis_connection = redis.from_url(url=redis_url, decode_responses=True)
