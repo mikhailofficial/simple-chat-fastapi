9b862ba9aab65b9f4bead346fc2ffeb4081587e2
mikhailofficial <asdgrghd@gmail.com>
2025-09-06 10:12:52 +0400

    feat(db): added index for users table

--- backend/src/database/models/user.py
+++ backend/src/database/models/user.py
@@ -6,9 +6,9 @@ from .base import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True)
-    hashed_password = Column(String)
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String, nullable=False)
 
 
 
